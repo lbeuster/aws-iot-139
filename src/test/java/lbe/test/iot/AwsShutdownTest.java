f599f2bd9c26ec9ba5bc09deea9accfc72583b85
Lars Beuster <l.beuster@cartelsol.com>
2021-03-04 09:46:21 +0100

    Remove mtls stuff.

--- src/test/java/lbe/test/iot/AwsShutdownTest.java
+++ src/test/java/lbe/test/iot/AwsShutdownTest.java
@@ -1,15 +1,8 @@
 package lbe.test.iot;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
 import org.junit.jupiter.api.Test;
 
-import com.google.common.io.Resources;
-
 import software.amazon.awssdk.crt.io.ClientBootstrap;
-import software.amazon.awssdk.crt.io.EventLoopGroup;
-import software.amazon.awssdk.crt.io.HostResolver;
 import software.amazon.awssdk.crt.mqtt.MqttClientConnection;
 import software.amazon.awssdk.iot.AwsIotMqttConnectionBuilder;
 
@@ -18,9 +11,7 @@ import software.amazon.awssdk.iot.AwsIotMqttConnectionBuilder;
  */
 class AwsShutdownTest {
 
-    private static final String CERT_RESOURCE = "client.crt";
-    private static final String KEY_RESOURCE = "client.key";
-    private static final String ENDPOINT = "endpoint-not-set";
+    private static final String ENDPOINT = "doesnt.matter";
 
     /**
      *
@@ -35,16 +26,12 @@ class AwsShutdownTest {
 
     private MqttClientConnection newClient() throws Exception {
 
-        String certificate = loadResourceAsString(CERT_RESOURCE);
-        String privateKey = loadResourceAsString(KEY_RESOURCE);
-        EventLoopGroup eventLoopGroup = new EventLoopGroup(1);
-        ClientBootstrap clientBootstrap = new ClientBootstrap(eventLoopGroup, new HostResolver(eventLoopGroup));
-        // ClientBootstrap clientBootstrap = new ClientBootstrap(null, null);
+        ClientBootstrap clientBootstrap = new ClientBootstrap(null, null);
         String clientId = "aws-iot-139";
 
         // @formatter:off
         AwsIotMqttConnectionBuilder builder = AwsIotMqttConnectionBuilder
-            .newMtlsBuilder(certificate, privateKey)
+            .newDefaultBuilder()
             .withEndpoint(ENDPOINT)
             .withClientId(clientId)
             .withBootstrap(clientBootstrap);
@@ -54,9 +41,5 @@ class AwsShutdownTest {
         MqttClientConnection mqttClient = builder.build();
         builder.close();
         return mqttClient;
-    }
-
-    private String loadResourceAsString(String resource) throws IOException {
-        return Resources.asCharSource(Resources.getResource(resource), StandardCharsets.UTF_8).read();
     }
 }
